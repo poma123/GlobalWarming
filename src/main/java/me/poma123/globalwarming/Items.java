f3f319e7a27cea755f1bfd5d7dc73aa0dcbadba6
poma123 <25465545+poma123@users.noreply.github.com>
2020-09-06 19:08:53 +0200

    Added Thermometer texture

---
+++ src/main/java/me/poma123/globalwarming/Items.java
@@ -0,0 +1,10 @@
+package me.poma123.globalwarming;
+
+import io.github.thebusybiscuit.slimefun4.utils.HeadTexture;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.Material;
+
+public class Items {
+
+    public static final SlimefunItemStack THERMOMETER = new SlimefunItemStack("THERMOMETER", "24fa511f2628d56a8c8691ac5df3e3f82716384514a5ea5bae3eda86f48ad6e1", "&eThermometer");
+}
