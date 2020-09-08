fc702c09098a2d9b24edbb2085e226d6cfeea0ae
poma123 <25465545+poma123@users.noreply.github.com>
2020-09-08 20:09:43 +0200

    Implemented night and storm temperature drop

--- src/main/java/me/poma123/globalwarming/utils/TemperatureUtils.java
+++ src/main/java/me/poma123/globalwarming/utils/TemperatureUtils.java
@@ -4,6 +4,7 @@ import me.poma123.globalwarming.GlobalWarming;
 import me.poma123.globalwarming.objects.Temperature;
 import me.poma123.globalwarming.objects.TemperatureType;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Biome;
 
 import java.util.Map;
@@ -11,6 +12,8 @@ import java.util.Map;
 public class TemperatureUtils {
     public static final String COLD = "❄";
     public static final String HOT = "☀";
+    private static final int NIGHT_TEMPERATURE_DROP = 10;
+    private static final int STORM_TEMPERATURE_DROP = 5;
 
     public static String getTemperatureString(Location loc, TemperatureType tempType) {
         Temperature temp = getTemperatureAtLocation(loc);
@@ -33,6 +36,7 @@ public class TemperatureUtils {
     }
 
     public static Temperature getTemperatureAtLocation(Location loc) {
+        World w = loc.getWorld();
         Biome b = loc.getBlock().getBiome();
         Map<Biome, Double> tempMap = GlobalWarming.getRegistry().getDefaultBiomeTemperatures();
         double celsiusValue = 15;
@@ -41,6 +45,21 @@ public class TemperatureUtils {
             celsiusValue = tempMap.get(b);
         }
 
+        if (!isDaytime(w)) {
+            celsiusValue = celsiusValue - NIGHT_TEMPERATURE_DROP;
+        } else if (isStorming(w)) {
+            celsiusValue = celsiusValue - STORM_TEMPERATURE_DROP;
+        }
+
         return new Temperature(celsiusValue);
+    }
+
+    public static boolean isDaytime(World world) {
+        long time = world.getTime();
+        return (time < 12300 || time > 23850);
+    }
+
+    public static boolean isStorming(World world) {
+        return !world.hasStorm() && !world.isThundering();
     }
 }
