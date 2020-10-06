c83c599870838f32b040455dd45c347aacfda844
poma123 <25465545+poma123@users.noreply.github.com>
2020-10-06 22:30:49 +0200

    Really small improvement

--- src/main/java/me/poma123/globalwarming/GlobalWarming.java
+++ src/main/java/me/poma123/globalwarming/GlobalWarming.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.logging.Level;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
