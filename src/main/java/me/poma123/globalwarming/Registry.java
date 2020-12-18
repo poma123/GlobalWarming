1ce52e8aa1ebc16a6e289f5bd8b19d0c16ce9f4f
poma123 <25465545+poma123@users.noreply.github.com>
2020-12-18 01:06:52 +0100

    [CI skip] Fix

--- src/main/java/me/poma123/globalwarming/Registry.java
+++ src/main/java/me/poma123/globalwarming/Registry.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.Locale;
 import java.util.logging.Level;
 
 import javax.annotation.Nonnull;
